a9f6f98ab9ba86b0d55bb6ec96b2944047abe821
wsl_tsyi <tsyi@kisti.re.kr>
2022-09-08 11:35:56 +0900

    dd

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     python_requires='>=3',
     include_data_package=True,
     packages=find_packages(exclude = []),
-    package_dir={'': 'tsyi_tflib'},
+    package_dir={'': ''},
     package_data={'':['data/*.txt']},
     data_files=[('', ['cfg1/text1.cfg', 'cfg2/text2.cfg'])],
     zip_safe=False,
