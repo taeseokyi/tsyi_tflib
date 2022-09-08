f67a104fa140e2ab37e5c7367baea2e13b201981
wsl_tsyi <tsyi@kisti.re.kr>
2022-09-08 10:07:55 +0900

    처음 올림

---
+++ setup.py
@@ -0,0 +1,22 @@
+from gettext import install
+from setuptools import setup, find_packages
+
+setup(
+    name='tsyi_tflib',
+    version='0.0.0',
+    description='tsyi pip install test',
+    url='https://github.com/taeseokyi/tf-official-common.git',
+    author='taeseok yi',
+    author_email='tsyi@kisti.re.kr',
+    license='taeseok yi',
+    python_requires='>=3',
+    include_data_package=True,
+    packages=find_packages(exclude = []),
+    package_dir={'':'src'}
+    package_data={'tsyi_tflib':['data/*.txt']},
+    data_files=[('config/cfg', ['cfg1/text1.cfg', 'cfg2/text2.cfg'])]
+    zip_safe=False,
+    install_requires=[
+        'numpy>=1.19.2'
+    ]
+)
